8022e13b40669e028d96584e84a10c48ef6a485d
jiajinda <jiajinda@yeah.net>
2023-05-21 13:55:02 -0400

    part 1 first commit

--- sieve0.c
+++ sieve0.c
@@ -13,23 +13,25 @@
 
 #define MIN(a,b)  ((a)<(b)?(a):(b))
 
+typedef long long LLong;
+
 int main (int argc, char *argv[])
 {
-   int    count;        /* Local prime count */
+   LLong  count;        /* Local prime count */
    double elapsed_time; /* Parallel execution time */
-   int    first;        /* Index of first multiple */
-   int    global_count; /* Global prime count */
-   int    high_value;   /* Highest value on this proc */
-   int    i;
+   LLong    first;        /* Index of first multiple */
+   LLong  global_count; /* Global prime count */
+   LLong    high_value;   /* Highest value on this proc */
+   LLong    i;
    int    id;           /* Process ID number */
    int    index;        /* Index of current prime */
-   int    low_value;    /* Lowest value on this proc */
+   LLong    low_value;    /* Lowest value on this proc */
    char  *marked;       /* Portion of 2,...,'n' */
-   int    n;            /* Sieving from 2, ..., 'n' */
+   LLong  n;            /* Sieving from 2, ..., 'n' */
    int    p;            /* Number of processes */
    int    proc0_size;   /* Size of proc 0's subarray */
-   int    prime;        /* Current prime */
-   int    size;         /* Elements in 'marked' */
+   LLong    prime;        /* Current prime */
+   LLong    size;         /* Elements in 'marked' */
 
    MPI_Init (&argc, &argv);
 
@@ -46,7 +48,7 @@ int main (int argc, char *argv[])
       exit (1);
    }
 
-   n = atoi(argv[1]);
+   n = atoll(argv[1]);
 
    /* Figure out this process's share of the array, as
       well as the integers represented by the first and
@@ -61,7 +63,7 @@ int main (int argc, char *argv[])
 
    proc0_size = (n-1)/p;
 
-   if ((2 + proc0_size) < (int) sqrt((double) n)) {
+   if ((2 + proc0_size) < (long long) sqrt((double) n)) {
       if (!id) printf ("Too many processes\n");
       MPI_Finalize();
       exit (1);
@@ -97,7 +99,7 @@ int main (int argc, char *argv[])
    count = 0;
    for (i = 0; i < size; i++)
       if (!marked[i]) count++;
-   if (p > 1) MPI_Reduce (&count, &global_count, 1, MPI_INT, MPI_SUM,
+   if (p > 1) MPI_Reduce (&count, &global_count, 1, MPI_LONG_LONG, MPI_SUM,
       0, MPI_COMM_WORLD);
 
    /* Stop the timer */
