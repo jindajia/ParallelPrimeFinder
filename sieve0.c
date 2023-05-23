5c0fdc83f96b3a2294e998cc94dc8b6d86c3bfcf
jiajinda <jiajinda@yeah.net>
2023-05-22 22:56:47 -0400

    Part3

--- sieve0.c
+++ sieve0.c
@@ -32,7 +32,8 @@ int main (int argc, char *argv[])
    LLong  proc0_size;   /* Size of proc 0's subarray */
    LLong  prime;        /* Current prime */
    LLong  size;         /* Elements in 'marked' */
-
+   char  *parent_primes;/* Primes to sieve */
+   LLong  parent_size;  /* Parent primes size*/
    MPI_Init (&argc, &argv);
 
    /* Start the timer */
@@ -60,9 +61,9 @@ int main (int argc, char *argv[])
       */
    
 
-   low_value = 3 + id*((n-1)/2)/p*2;
-   high_value = 1 + (id+1)*((n-1)/2)/p*2;
-   size = (high_value - low_value)/2 + 1;
+   low_value = 3;
+   high_value = (n-1)/2/p*2 + 1;
+   parent_size = ((n-1)/2/p*2 - 2)/2 + 1;
 
    /* Bail out if all the primes used for sieving are
       not all held by process 0 */
@@ -75,7 +76,47 @@ int main (int argc, char *argv[])
       exit (1);
    }
 
+   /* Allocate parent_primes to seive */
+   parent_primes = (char *) malloc (parent_size);
+
+   if (parent_primes == NULL) {
+      printf ("Cannot allocate enough memory\n");
+      MPI_Finalize();
+      exit (1);
+   }
+
+   for (i = 0; i < parent_size; i++) parent_primes[i] = 0;
+
+   /* 
+      sequential mark parent_primes 
+      low_value for parent_primes is 3
+      high_value for parent_primes is (n-1)/2/p*2 + 1
+   */
+   prime = 3;
+   index = 0;
+   do{
+      if (prime * prime > low_value)
+         first = ((prime * prime) - low_value) / 2;
+      else {
+            if ( (prime - (low_value) % prime) % 2 == 0) {
+               first = (prime - (low_value) % prime) / 2;
+            } else {
+               first = (prime * 2 - (low_value) % prime) / 2;
+            }
+      }
+      for (i = first; i < parent_size; i += prime) parent_primes[i] = 1;
+      while (parent_primes[++index]);
+      prime = index * 2 + 3;
+   } while (prime * prime <= high_value);
+
+
    /* Allocate this process's share of the array. */
+
+   low_value = 3 + id*((n-1)/2)/p*2;
+   high_value = 1 + (id+1)*((n-1)/2)/p*2;
+   size = (high_value - low_value)/2 + 1;
+   index = 0;
+   prime = index * 2 + 3;
 
    marked = (char *) malloc (size);
 
@@ -86,8 +127,7 @@ int main (int argc, char *argv[])
    }
 
    for (i = 0; i < size; i++) marked[i] = 0;
-   if (!id) index = 0;
-   prime = 3;
+
    do {
       if (prime * prime > low_value)
          first = (prime * prime - low_value) / 2;
@@ -102,11 +142,9 @@ int main (int argc, char *argv[])
          }
       }
       for (i = first; i < size; i += prime) marked[i] = 1;
-      if (!id) {
-         while (marked[++index]);
-         prime = index * 2 + 3;
-      }
-      if (p > 1) MPI_Bcast (&prime,  1, MPI_INT, 0, MPI_COMM_WORLD);
+      //find next prime to seive
+      while (parent_primes[++index]);
+      prime = index * 2 + 3;
    } while (prime * prime <= n);
    count = 0;
    for (i = 0; i < size; i++)
